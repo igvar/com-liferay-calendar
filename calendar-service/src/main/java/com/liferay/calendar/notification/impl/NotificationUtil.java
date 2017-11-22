brandizzi/liferay-portal

LPS-74454 Keep methods signature to avoid major version bump.

modules/apps/forms-and-workflow/calendar/calendar-service/src/main/java/com/liferay/calendar/notification/impl/NotificationUtil.java
@@ -129,6 +129,16 @@ public class NotificationUtil {
 
 	public static void notifyCalendarBookingRecipients(
 			CalendarBooking calendarBooking, NotificationType notificationType,
+			NotificationTemplateType notificationTemplateType, User senderUser)
+		throws Exception {
+
+		notifyCalendarBookingRecipients(
+			calendarBooking, notificationType, notificationTemplateType,
+			senderUser, null);
+	}
+
+	public static void notifyCalendarBookingRecipients(
+			CalendarBooking calendarBooking, NotificationType notificationType,
 			NotificationTemplateType notificationTemplateType, User senderUser,
 			ServiceContext serviceContext)
 		throws Exception {
@@ -230,7 +240,7 @@ public class NotificationUtil {
 			NotificationTemplateContext notificationTemplateContext =
 				NotificationTemplateContextFactory.getInstance(
 					notificationType, NotificationTemplateType.REMINDER,
-					calendarBooking, user, null);
+					calendarBooking, user);
 
 			notificationSender.sendNotification(
 				user.getEmailAddress(), user.getFullName(),

igvar/com-liferay-calendar

calendar-service/src/main/java/com/liferay/calendar/notification/impl/NotificationUtil.java
@@ -129,6 +129,16 @@ public class NotificationUtil {
 
 	public static void notifyCalendarBookingRecipients(
 			CalendarBooking calendarBooking, NotificationType notificationType,
+			NotificationTemplateType notificationTemplateType, User sender)
+		throws Exception {
+
+		notifyCalendarBookingRecipients(
+			calendarBooking, notificationType, notificationTemplateType, sender,
+			null);
+	}
+
+	public static void notifyCalendarBookingRecipients(
+			CalendarBooking calendarBooking, NotificationType notificationType,
 			NotificationTemplateType notificationTemplateType, User sender,
 			ServiceContext serviceContext)
 		throws Exception {
@@ -206,7 +216,7 @@ public class NotificationUtil {
 			NotificationTemplateContext notificationTemplateContext =
 				NotificationTemplateContextFactory.getInstance(
 					notificationType, NotificationTemplateType.REMINDER,
-					calendarBooking, user, null);
+					calendarBooking, user);
 
 			notificationSender.sendNotification(
 				user.getEmailAddress(), user.getFullName(),